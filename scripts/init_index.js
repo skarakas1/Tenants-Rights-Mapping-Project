0ed79f233d131d775872baafbbaf99bd317a830f
skarakas1 <skarakas21@ucla.edu>
2021-06-10 00:13:11 -0700

    we are very close to getting this working
    Co-authored-by: albertkun <albertkun@users.noreply.github.com>
    Co-authored-by: Ray Hsiao <rh5140@users.noreply.github.com>

--- scripts/init_index.js
+++ scripts/init_index.js
@@ -1,11 +1,6 @@
- //variable declarations
+//HELLO WE ARE GLOBAL VARIABLES
 let id = 0;//create a unique id for each survey response object
-//let objectArray = [];//create an array to store the objects
-    //^i don't think we ever use this array
 
-//-----------------------------------------
-//--------------------MAP LAYERS----------
-//-----------------------------------------
 //create Leaflet feature group layers
 let totalResponseLayer = L.featureGroup();
 let harassmentLayer = L.featureGroup();
@@ -20,6 +15,24 @@ let layers = {
     "Community solutions": resourcesLayer
 }
 
+let globalZip = "this is broken!";
+
+//declarations for turf stuff
+let allLayers;
+// this is the boundary layer located as a geojson in the /data/ folder 
+const boundaryLayer = "./data/la_zipcodes.geojson"
+let boundary; // place holder for the data
+let collected; // variable for turf.js collected points 
+let allPoints = []; // array for all the data points
+
+//HTML DOM declarations
+let sideBarNav = document.getElementById("sidebarnav");
+let sideBarText = document.getElementById("sidebartext");
+
+// Variable tracking current sidebar tab changes based on button clicked
+// might not be the intelligent solution
+// right now, currentTab is not changing at all
+let currentTab = 0
 //-----------------------------------------
 //--------------------MAP SCRIPTS----------
 //-----------------------------------------
@@ -49,9 +62,7 @@ let CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/
 //set basemap
 CartoDB_Positron.addTo(map);
 
-//-----------------------------------------
-//--------------DATA CLEANING/SHEETS API---
-//-----------------------------------------
+//DATA CLEANING & SHEETS AP
 //get the datas as json
 const url = "https://spreadsheets.google.com/feeds/list/16F-aIZ0PutDur9tXTy92JD7rFrHd3YHZB1wCsh9Gs04/on8014x/public/values?alt=json"
 fetch(url)
@@ -79,7 +90,11 @@ function processData(theData){
       formattedData.push(formattedRow)
     }
     formattedData.forEach(createObject)//create object and send to global array
-    //console.log(objectArray)//log the objects
+    // step 1: turn allPoints into a turf.js featureCollection
+    thePoints = turf.featureCollection(allPoints)
+
+    // step 2: run the spatial analysis
+    getBoundary(boundaryLayer)
 }
 
 //create survey response object
@@ -103,19 +118,10 @@ function createObject(data, id){
         "email": data.pleaseenteryouremailaddress
     }//THIS IS THE SURVEY DATA OBJECT
 
-    // step 1: turn allPoints into a turf.js featureCollection
-    thePoints = turf.featureCollection(allPoints)
-    // console.log(thePoints)
-
-    // step 2: run the spatial analysis
-    getBoundary(boundaryLayer)
-
     //turf requires lng,lat format other than lat,lng
     let thisPoint = turf.point([Number(data.lng),Number(data.lat)],{thisData})// create the turfJS point
     allPoints.push(thisPoint)// put all the turfJS points into `allPoints`
 
-    //objectArray.push(thisData);//add object to global array objectArray
-    //^i don't think we ever use this array
     popMap(thisData);
 }
 
@@ -131,10 +137,12 @@ let circleOptions = {
     opacity: 1,
     fillOpacity: .25
 }
+
 //function for add markers & assign layer   
 function addMarkers(data, group){
     group.addLayer(L.circle([data.lat,data.lng],circleOptions).addTo(map))//REMOVED 'bind popup'
 }
+
 function popMap(object){//function to populate the map
     addMarkers(object, totalResponseLayer);//add marker/layer regardless for total responses
     if(object.harassmentYN){//add markers/layer for 'have experienced harassment'
@@ -154,29 +162,8 @@ function popMap(object){//function to populate the map
     }
 }
 
-//-----------------------------------------
-//--------------------ZIPCode Boundary Layer
-//-----------------------------------------
-//declarations for turf stuff
-let allLayers;
-// this is the boundary layer located as a geojson in the /data/ folder 
-const boundaryLayer = "../data/la_zipcodes.geojson"
-let boundary; // place holder for the data
-let collected; // variable for turf.js collected points 
-let allPoints = []; // array for all the data points
-
-//HTML DOM declarations
-let sideBarNav = document.getElementById("sidebarnav");
-let sideBarText = document.getElementById("sidebartext");
-
-// Variable tracking current sidebar tab changes based on button clicked
-// might not be the intelligent solution
-// right now, currentTab is not changing at all
-let currentTab = 0
-
 // function to create Tab Menu
 function createTabMenu(id,readableName,tabIndex){
-    console.log('hi')
     let tabMenu = document.getElementById('sidebarnav')
     let thisTabButton = document.createElement('button')
     thisTabButton.id = id
@@ -187,8 +174,6 @@ function createTabMenu(id,readableName,tabIndex){
     // let me know if you get stuck!
     thisTabButton.addEventListener('click',function(){
         currentTab = tabIndex
-        // check to console to see currentTab change onClick
-        console.log('the current tab is: '+currentTab)
         // Todo: create a function to update the data here
         setContentToTab(currentTab)
         // i believe you run 
@@ -211,18 +196,17 @@ const tabIndices = {
 }
 
 function setContentToTab(tabindex){
-//function setContentToTab(tabindex,targetClass){ // I don't see when targetClass would get used
+    // need to filter by zipcode
     let lookUpTarget = tabIndices[tabindex]
-    console.log("lookUpTarget is " + lookUpTarget)
 
     // creates array
     let divsToChange = document.getElementsByClassName(lookUpTarget)
 
-    // Display current tab
-    let numDivs = divsToChange.length // Number of surveys, so works for all divs
+        // Display current tab
+    var numDivs = divsToChange.length // Number of surveys, so works for all divs
     for (i = 0; i < numDivs; i++)
     {
-        divsToChange[i].style.display = "grid"
+            divsToChange[i].style.display = "grid"
     }
 
     // Hide other tabs
@@ -232,7 +216,6 @@ function setContentToTab(tabindex){
         {
             lookUpTarget = tabIndices[i]
             divsToChange = document.getElementsByClassName(lookUpTarget)
-            console.log("i = " + i + " lookup=" +lookUpTarget)
             for (j = 0; j < numDivs; j++)
             {
                 divsToChange[j].style.display = "none"
@@ -249,40 +232,15 @@ createTabMenu('securityButton','Housing insecurity stories',1)
 createTabMenu('resourcesButton','Community solutions',2)
 createTabMenu('nonRentersButton','Nonrenters',3)
 
-// document.getElementById('harassmentButton').addEventListener("click", function(){
-//     currentTab = 0;
-// }
-// );
-// document.getElementById('securityButton').addEventListener("click", function(){
-//     currentTab = 1;
-// }
-// );
-// document.getElementById('resourcesButton').addEventListener("click", function(){
-//     currentTab = 2;
-// }
-// );
-// document.getElementById('nonRentersButton').addEventListener("click", function(){
-//     currentTab = 3;
-// }
-// );
-
-// 1.	In the getSurveyInfo function, create divs for each survey data with a unique ID (UID) and also separate the content into divs with separate tab-index data attributes. What I mean is you want to create a `div` for the harassment data, a `div` for resources, and so forth. Important: make sure that each div has the corresponding data attribute with the ID set in the createTabMenu function. i.e. div for harassment stories should be in the div with the data attribute 0. An example output should be: `<div id=”survey_0” tab-index=”1”> here are the resources I recommend for tenants help</div>` 
-
-//Hint: You will need to create a unique ID for each survey data and assign it to each div.
-//2.	Modify the event listener on line 185, so that when a user clicks on the tab, that it sets that content’s UID to the ID of the tab-index of the corresponding tab, i.e. when I click on resources, it will show me this data’s UID and the data in data-attribute for “1”.
-//Hint: You will need two parameters, 1 for the tab index and 1 for the survey UID, then you will look up each to return the appropriate `div`.
-
-
-
 
 function getSurveyInfo(survey){
-    console.log('survey')
-    console.log(survey)
     // let result = survey.resources
     // let result;
 
     let harassmentTab, securityTab, resourcesTab, notRentingTab;
     let thisZipcode = document.createElement('div')
+    thisZipcode.setAttribute('class',globalZip)
+    
 
     // if renter
     // generate tabs for harassment story/security story/resources
@@ -329,7 +287,6 @@ function getSurveyInfo(survey){
     }
 
     let surveyID = "survey_" + survey.id
-    console.log(surveyID)
     // Make the divs contain the data
     // how to make div id = a variable? how to make inner text a variable???
     //<div id=surveyID tab-index="0">harassmentTab</div> leads to problems
@@ -342,6 +299,9 @@ function getSurveyInfo(survey){
     thisSurveysDiv.id = surveyID
     allSurveyContentDiv.appendChild(thisSurveysDiv)
     let targetContainer = document.getElementById(surveyID)
+    
+    
+
     
     // harassmentTabContent corresponds w/ 0
     let harassmentTabContent = document.createElement('div')
@@ -371,37 +331,12 @@ function getSurveyInfo(survey){
     notRentingTabContent.setAttribute("tabindex","3")
     targetContainer.appendChild(notRentingTabContent)
 
-    // I THINK THE BELOW CODE IS NOT USEFUL ANYMORE
-    // if (currentTab == 0)
-    // {
-    //     thisZipcode.textContent = harassmentTab // later change name maybe bc confusing
-    // }
-    // else if (currentTab == 1)
-    // {
-    //     thisZipcode.textContent = securityTab
-    // }
-    // else if (currentTab == 2)
-    // {
-    //     thisZipcode.textContent = resourcesTab
-    // }
-    // else if (currentTab == 3)
-    // {
-    //     thisZipcode.textContent = notRentingTab
-    // }
-    // else
-    // {
-    //     thisZipcode.textContent = "ERROR"
-    // }
-    console.log('thisZipcode')
-    console.log(thisZipcode)
-    // console.log(thisZipcode.innerHTML)
     sideBarText.appendChild(thisZipcode)
     return thisZipcode//.textContent
 }
 
 //function for clicking on polygons
 function onEachFeature(feature, layer) {
-    // console.log(feature.properties)
     let text;
     // do this only if there is survey data in a zipcode
     if (feature.properties.values.length > 0) {//within this if statement onEachFeature loops thru everything 
@@ -413,6 +348,8 @@ function onEachFeature(feature, layer) {
         let count = feature.properties.values.length
         // console.log(count) // see what the count is on click
         //this is thesurvey data/zip
+
+        layer.on('click',function(){zipCodeClick(zipcode)})
     }
     else{
         text = "We don't have any data for this zipcode yet--please fill out the <a href='survey.html'> survey!</a>"
@@ -423,82 +360,35 @@ function onEachFeature(feature, layer) {
         }
 }
 
+function zipCodeClick(zipcode){
+    globalZip = zipcode;
+    console.log(globalZip)
+}
+
 //turf.js geoprocessing and polygons
 function getBoundary(layer){
     fetch(layer)
     .then(response => {
         return response.json();
         })
-    .then(data =>{
+    .then(boundary =>{
                 //set the boundary to data
-                boundary = data
                 // run the turf collect geoprocessing
-                collected = turf.collect(boundary, thePoints, 'thisData', 'values');
-                // console.log('collected')
-                // console.log(collected)
-                // here is the geoJson of the `collected` result:                
-                L.geoJson(collected,{onEachFeature: onEachFeature,style:function(feature){
-                    // console.log(feature)
+                collected = turf.collect(boundary, thePoints, 'thisData', 'values');                
+                 // here is the geoJson of the `collected` result:
+                 L.geoJson(collected,{onEachFeature: onEachFeature,style:function(feature){
                     if (feature.properties.values.length > 0) {
-                        //feature.addEventListener(onclick,popSidebar(thisData))
-                        return {
-                            color: "orange",stroke: true
-                        };
+                        return {color: "orange",stroke: false};
                     }
-                    else{
-                        // make the polygon gray and blend in with basemap if it doesn't have any values
-                        return{
-                            opacity:0,color:"#efefef"
-                        }
+                    else{// make the polygon gray and blend in with basemap if it doesn't have any values
+                        return{opacity:0,color:"#efefef"};
                     }
                 }
                 // add the geojson to the map
-                    }).addTo(map)
-        }
-    )   
+            }).addTo(map)
+        })   
 }
-//console.log(boundary)
-//-----------------------------------------
-//--------------------SIDEBAR--------------
-//-----------------------------------------
 
-//THIS IS ALL PSEUDOCODE...
-//function to determine which id story to display
-// function getStoryID(zipcode?, object array?){
-//     for(item in objectArray){
-//         if(item.zipcode){    
-//     }
-// }
-// //send each object thru sidebar function?
-// function createSidebar(objectArray, index){
-//     objectArray.forEach(popSidebar);
-// }
-// //
-// function popSidebar(object){
-//     if(object.insecurity){
-//         //add tab & text
-//     }
-//     if(object.harassment){ 
-//         //add tab & text
-//     }
-//     if(object.reasons){
-//         //add tab & text + upvote/downvote
-//     }
-// }
-
-// //--------------------------ALBERT'S UPVOTE FUNCTION
-// function getUpvotes(zipField,communityResourceID){
-//     let url = 'https://docs.google.com/forms/d/e/1FAIpQLSevye7kmOaEeC879skTARFepAwyCus66WqQHEha0_FzY88Z-A/viewform?usp=pp_url'
-//     let param1 = '&entry.1155798845='+zipField
-//     let param2 = `&entry.725289503=kfHDJKhdsjkhfdjkf=`+communityResourceID
-//     let iframeUrl = url + param1 + param2
-//     // someone clicks on CR
-//     // then show them the up vote form
-// }
-
-//-----------------------------------------
-//--------------------LAYER CONTROL----------
-//-----------------------------------------
 //add layer control box
 allLayers = L.featureGroup([totalResponseLayer, harassmentLayer, insecureLayer, resourcesLayer]);
 function layerControl(layerGroup){
@@ -528,31 +418,3 @@ const search = new GeoSearch.GeoSearchControl({
 map.addControl(search);
 //console.log(GeoSearch.OpenStreetMapProvider)
 
-//-----------------------------------------
-//--------------------MODAL----------
-//-----------------------------------------
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// window.addEventListener('load', (event) => {
-//   modal.style.display = "block";
-// });
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
